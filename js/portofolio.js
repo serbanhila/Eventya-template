81c8a06565e1691d1334e604d8d8c291a6c247ba
Serban Hila <serban.hila@eventya.net>
2021-09-28 16:12:41 +0300

    ≈

---
+++ js/portofolio.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $(".filter").click(function(){
+        var attr = $(this).attr("data-filter")
+
+        $(".filter").removeClass("active");
+        $(this).addClass("active");
+
+        $(".item").hide();
+        if(attr == "church"){
+            $("." + attr).show();
+        }
+        else if(attr == "community-mobile-app"){
+            $("." + attr).show();
+        }
+        else if(attr == "content-creators-podcasters"){
+            $("." + attr).show();
+        }
+        else if(attr == "festival"){
+            $("." + attr).show();
+        }
+        else if(attr == "free-time-magazine-app"){
+            $("." + attr).show();
+        }
+        else if(attr == "museum"){
+            $("." + attr).show();
+        }
+        else if(attr == "smart-city-app"){
+            $("." + attr).show();
+        }
+        else if(attr == "theater-philharmonic"){
+            $("." + attr).show();
+        }
+        else if(attr == "tourism-travel-mobile-app"){
+            $("." + attr).show();
+        }
+        else{
+            $(".item").show();
+        }
+    })
+});
